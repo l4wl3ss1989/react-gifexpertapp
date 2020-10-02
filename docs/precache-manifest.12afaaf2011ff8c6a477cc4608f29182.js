9f8d8092c4389b276142a97f47a489959baf1c22
L4wl3sS <l4wl3ss1989@gmail.com>
2020-10-02 09:17:21 +0200

    renamed build to docs

---
+++ docs/precache-manifest.12afaaf2011ff8c6a477cc4608f29182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77757ac99ef00323d48814cd1ec8e6c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a4fde7b95f1ca4f11b",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "99918298aacfbfea9aa1",
+    "url": "/static/js/2.4e9c1a5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e9c1a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a4fde7b95f1ca4f11b",
+    "url": "/static/js/main.21a349cb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
